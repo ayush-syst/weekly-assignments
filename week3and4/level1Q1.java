7e6833eddd7d8b5b8f845a80fbc2c16d3dff0253
ayush-syst <2389ayush@gmail.com>
2026-03-31 14:33:33 +0530

    done L1Q1

---
+++ week3and4/level1Q1.java
@@ -0,0 +1,15 @@
+package week3and4;
+
+public class level1Q1 {
+    
+    public static void main(String[] args) {
+        int num = 10;
+        if (num % 5 == 0) {
+            System.out.println(num + " is divisible by 5.");
+        } else {
+            System.out.println(num + " is not divisible by 5.");
+        }
+    }
+    
+}
+
