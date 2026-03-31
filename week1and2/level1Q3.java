726150ca41805fb44e38b9d888c2dcfda12788bd
ayush-syst <2389ayush@gmail.com>
2026-03-31 14:05:25 +0530

    done L1Q3

---
+++ week1and2/level1Q3.java
@@ -0,0 +1,11 @@
+package week1and2;
+
+public class level1Q3 {
+    
+
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
