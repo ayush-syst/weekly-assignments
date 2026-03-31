7b7a7fb8c5a61e02b88a5ffccab63c7f99e8ec63
ayush-syst <2389ayush@gmail.com>
2026-03-31 15:46:11 +0530

    done L1Q2

---
+++ week3and4/level1Q2.java
@@ -0,0 +1,21 @@
+package week3and4;
+import java.util.Scanner;
+public class level1Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number 1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter a number 2: ");
+        int num2 = scanner.nextInt();
+        System.out.println("Enter a number 3: ");
+        int num3 = scanner.nextInt();
+        if (num1 < num2 && num1 < num3) {
+            System.out.println(num1 + " is the smallest number.");
+        } else if (num2 < num1 && num2 < num3) {
+            System.out.println(num2 + " is the smallest number.");
+        } else {
+            System.out.println(num3 + " is the smallest number.");
+        }
+        scanner.close();
+    }
+}
