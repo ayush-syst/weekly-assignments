56d902abb97797cf497d86605667b134af506774
ayush-syst <2389ayush@gmail.com>
2026-03-31 15:55:55 +0530

    done L2Q3

---
+++ week3and4/level2Q3.java
@@ -0,0 +1,16 @@
+package week3and4;
+import java.util.Scanner;
+public class level2Q3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+        scanner.close();
+        System.out.println("Multiplication table of " + number + "from 6 to 9:");
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+
+}
+}
