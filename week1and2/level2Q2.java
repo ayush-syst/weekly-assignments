2f8eaf76542f972f0f05dd0f238f3a1f85f66246
ayush-syst <2389ayush@gmail.com>
2026-03-31 14:15:39 +0530

    done L2Q2

---
+++ week1and2/level2Q2.java
@@ -0,0 +1,18 @@
+package week1and2;
+import java.util.Scanner;
+public class level2Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base of the triangle in cm: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height of the triangle in cm: ");
+        double height = sc.nextDouble();
+
+        double areaCm = 0.5 * base * height;
+        double areaIn = areaCm / (2.54 * 2.54);
+
+        System.out.println("The Area of the triangle in sq in is " + areaIn + " and sq cm is " + areaCm);
+
+        sc.close();
+    }
+}
